76bf406d43033bb99363576e3eb53be7b8d453d4
Maxim <sibmaks.khv@mail.ru>
2022-08-23 23:38:46 +1000

    Add Task40 and TestingArrays

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -29,14 +29,24 @@ int B = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите первое число: ");
 int C = Convert.ToInt32(Console.ReadLine());
 
-bool IsTriangleExist(A, B, C)
+
+bool res = IsTriangleExist(A, B, C);
+if(res == true)// такой треугольник может существовать
 {
-    if (A < B + C && B < A + C && C < A + B)
+    Console.WriteLine("Треугольник с такими сторонами существует");
+}
+else
+{
+    Console.WriteLine("Треугольник с такими сторонами не существует");
+}
+
+bool IsTriangleExist(int a, int b, int c)
+{
+    bool isExist = false; //такой треугольник не существует
+    if (a < b + C && b < a + c && c < a + b)
     {
-        Console.WriteLine("Треугольник с такими сторонами существует");
+        isExist = true; // такой треугольник может существовать
     }
-    else
-    {
-        Console.WriteLine("Треугольник с такими сторонами не существует");
-    }
+    return isExist;
+    
 }
---
+++ TestingArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿const int ARRAY_SIZE = 6;
+const int LEFT_RANGE = -10;
+const int RIGHT_RANGE = 10;
+
+int[] array = FillArrayWithRandomNumbers(ARRAY_SIZE, LEFT_RANGE, RIGHT_RANGE);
+int[] copyArray = array;
+
+array[0] = 100;
+Console.WriteLine($"Исходный массив: {string.Join(", ", array)}");
+Console.WriteLine($"Скопированный массив: {string.Join(", ", copyArray)}");
+
+//метод возвращает массив на size элементов
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(leftRange, rightRange + 1);
+    }
+
+    return array;
+}
+
